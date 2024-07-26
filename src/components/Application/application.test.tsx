f47e8c5897f2905063d3336e545a0c1eb880e98f
jrazap <elazapm@gmail.com>
2024-07-26 19:34:47 +0200

    RTL Queries

--- src/components/Application/application.test.tsx
+++ src/components/Application/application.test.tsx
@@ -2,9 +2,70 @@ import { render, screen } from "@testing-library/react";
 import Application from "./application";
 
 describe("Application", () => {
-  test("component rendered", () => {
+  test("renders correctly", () => {
     render(<Application />);
-    const nameInput = screen.getByRole('textbox')
-    expect(nameInput).toBeInTheDocument();
+    // Title
+    const pageHeading = screen.getByRole("heading", {
+      name: "Application form",
+      level: 1,
+    });
+    expect(pageHeading).toBeInTheDocument();
+
+    // Subtitle
+    const sectionHeading = screen.getByRole("heading", {
+      name: "Enter your data",
+      level: 3,
+    });
+    expect(sectionHeading).toBeInTheDocument();
+
+    // Paragraph
+    const paragraph_1 = screen.getByText(
+      /it will be used for testing purposes only/i
+    );
+    const paragraph_2 = screen.getByTestId("custom-element");
+    expect(paragraph_1).toBeInTheDocument();
+    expect(paragraph_2).toBeInTheDocument();
+
+    // Image 
+    const image_1 = screen.getByAltText("image placeholder");
+    const image_2 = screen.getByTitle("image title");
+    expect(image_1).toBeInTheDocument();
+    expect(image_2).toBeInTheDocument();
+
+    // Name Input
+    const nameInput_1 = screen.getByRole("textbox", {
+      name: "Name",
+    });
+    const nameInput_2 = screen.getByLabelText("Name", {
+      selector: "input",
+    });
+    expect(nameInput_1).toBeInTheDocument();
+    expect(nameInput_2).toBeInTheDocument();
+
+    // Bio Input
+    const bioInput_1 = screen.getByRole("textbox", {
+      name: "Bio",
+    });
+    const bioInput_2 = screen.getByPlaceholderText("Enter Bio");
+    expect(bioInput_1).toBeInTheDocument();
+    expect(bioInput_2).toBeInTheDocument();
+
+    // Location Input
+    const locationInput_1 = screen.getByRole("combobox", {
+      name: "Location",
+    });
+    const locationInput_2 = screen.getByDisplayValue("New York");
+    expect(locationInput_1).toBeInTheDocument();
+    expect(locationInput_2).toBeInTheDocument();
+
+    // Terms Input
+    const termsInput = screen.getByRole("checkbox", {
+      name: "Terms",
+    });
+    expect(termsInput).toBeInTheDocument();
+
+    // Submit Button
+    const submitButton = screen.getByRole("button");
+    expect(submitButton).toBeInTheDocument();
   });
 });
--- src/components/Application/application.tsx
+++ src/components/Application/application.tsx
@@ -2,48 +2,96 @@ import React from "react";
 
 const Application = () => {
   return (
-    <form className="my-4">
+    <div className="application my-4">
       <div className="container">
-        <h2 className="mb-4 display-4">Form Example</h2>
-        <div className="form-group mb-4">
-          <label htmlFor="name" className="form-label">
-            Name:
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            name="name"
-            placeholder="Enter name"
-          />
-        </div>
+        <h1 className="mb-4 fs-1 fw-bold text-capitalize">Application form</h1>
+        <h3 className="fs-3 fw-normal text-capitalize">Enter your data</h3>
+        <p className="text-muted" data-testid="custom-element">
+          Please fill out this form carefully as it will be used for testing
+          purposes only.
+        </p>
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="img-container">
+              <img
+                src="https://via.placeholder.com/200x200"
+                className="img-fluid"
+                alt="image placeholder"
+                title="image title"
+              />
+            </div>
+          </div>
+          <form className="col-lg-6">
+            <div className="form-group mb-4">
+              <label htmlFor="name" className="form-label">
+                Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                name="name"
+                placeholder="Name"
+                aria-label="Name"
+              />
+            </div>
 
-        <div className="form-group mb-4">
-          <label htmlFor="location" className="form-label">
-            Location:
-          </label>
-          <select name="location" id="location" className="form-select">
-            <option value="">Select...</option>
-            <option value="New York">New York</option>
-            <option value="Los Angeles">Los Angeles</option>
-            <option value="Chicago">Chicago</option>
-            <option value="Houston">Houston</option>
-            <option value="San Francisco">San Francisco</option>
-          </select>
-        </div>
+            <div className="form-group mb-4">
+              <label htmlFor="name" className="form-label">
+                Bio
+              </label>
+              <textarea
+                rows={5}
+                className="form-control"
+                id="bio"
+                name="bio"
+                placeholder="Enter Bio"
+                aria-label="Bio"
+              />
+            </div>
 
-        <div className="form-group mb-4">
-          <label htmlFor="terms" className="form-label">
-            <input type="checkbox" name="terms" id="terms" />
-            <span className="ms-2">I agree to the terms and conditions</span>
-          </label>
-        </div>
+            <div className="form-group mb-4">
+              <label htmlFor="location" className="form-label">
+                Location
+              </label>
+              <select
+                name="location"
+                id="location"
+                className="form-select"
+                aria-label="Location"
+                value="New York"
+                onChange={() => {}}
+              >
+                <option value="">Select...</option>
+                <option value="New York">New York</option>
+                <option value="Los Angeles">Los Angeles</option>
+                <option value="Chicago">Chicago</option>
+                <option value="Houston">Houston</option>
+                <option value="San Francisco">San Francisco</option>
+              </select>
+            </div>
 
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
+            <div className="form-group mb-4">
+              <label htmlFor="terms" className="form-label">
+                <input
+                  type="checkbox"
+                  name="terms"
+                  id="terms"
+                  aria-label="Terms"
+                />
+                <span className="ms-2">
+                  I agree to the terms and conditions
+                </span>
+              </label>
+            </div>
+
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
-    </form>
+    </div>
   );
 };
 
